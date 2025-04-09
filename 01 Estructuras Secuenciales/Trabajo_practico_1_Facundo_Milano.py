7138b4f92ead648aebbe2d7be3b66dcac8ace2b7
fmilano86 <157241079+fmilano86@users.noreply.github.com>
2025-04-09 08:46:31 -0300

    Se subio el TP 1

---
+++ 01 Estructuras Secuenciales/Trabajo_practico_1_Facundo_Milano.py
@@ -0,0 +1,79 @@
+#Ejercicio 1
+
+#print("Hola mundo!")
+
+#Ejercicio 2
+
+#nombre = input("Ingrese su nombre: ")
+#print(f"Hola, {nombre}!")
+
+#Ejercicio 3
+
+#nombre=input("Ingrese su nombre: ")
+#apellido=input("Ingrese su apellido: ")
+#edad=input("Ingrese su edad: ")
+#lugar=input("Ingrese su lugar de residencia: ")
+
+#print(f"Soy {nombre} {apellido}, tengo {edad} anios y vivo en {lugar} ")
+
+#Ejercicio 4
+
+#radio=(int(input("Ingrese el radio del circulo: ")))
+
+#area=3.14*radio**2
+#perimetro=2*3.14*radio
+
+#print(f"El perimetro del cirrculo es {perimetro} y su area {area}")
+
+#Ejercicio 5
+
+#segundos=int(input("Ingrese una cantidad de segundos: "))
+#horas=segundos/3600
+#print(f"La cantidad de segundo ingresada equivale a {horas} horas")
+
+#Ejercicio 6
+
+#numeromult=int(input("Ingrese un numero: "))
+#print(numeromult*2)
+#print(numeromult*3)
+#print(numeromult*4)
+#print(numeromult*5)
+#print(numeromult*6)
+#print(numeromult*7)
+#print(numeromult*8)
+#print(numeromult*9)
+#print(numeromult*10)
+
+#Ejercicio 7
+
+#num1=int(input("Ingrese un numero entero distinto de 0: "))
+#num2=int(input("Ingrese un numero entero distinto de 0: "))
+
+#print(f"La suma de los numeros ingresados es igual a {num1+num2}")
+#print(f"La multiplicacion de los numeros es igual a {num1*num2}")
+#print(f"La division del primer numero ingresado sobre el segundo es {num1/num2}")
+#print(f"La division del segundo numero ingresado sobre el primero es {num2/num1}")
+#print(f"El resultado del primer numero ingresado menos el segundo es {num1-num2}")
+#print(f"El resultado del segundo numero ingresado menos el primero es {num2-num1}")
+
+#Ejercicio 8
+
+#peso=int(input("Ingrese su peso: "))
+#altura=float(input("Ingrese su altura expresado en mts: "))
+#imc=peso/(altura**2)
+
+#print(f"Su Indice de Masa Corporal es {imc}")
+
+#Ejercicio 9
+
+#celsius=int(input("Ingrese la temperatura en grados Celsius: "))
+#fahrenheit=((9/5)*celsius)+32
+#print(f"La temperatura ingresada es de {fahrenheit} grados Fahrenheit")
+
+#Ejercicio 10
+
+numprom1=int(input("Ingrese el primer numero: "))
+numprom2=int(input("Ingrese el segundo numero: "))
+numprom3=int(input("Ingrese el tercer numero: "))
+prom=(numprom1+numprom2+numprom3)/3
+print(f"El promedio de los numeros ingresados es {prom}")
